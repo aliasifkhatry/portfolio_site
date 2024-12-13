25e8e0d8208244cd2605a6ad70895bc2f80b11db
Ali Asif <ali.asifkhatry@gmail.com>
2024-12-13 18:07:32 +0500

    adjusted text

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React from "react";
 
 interface AboutProps {
@@ -14,18 +12,18 @@ const About: React.FC<AboutProps> = ({
   thirdRowHeight = 140,
 }) => {
   const firstRowSections = [
-    { title: "Left Sub", description: "First half of left section" },
-    { title: "Right Sub", description: "Second half of left section" },
+    { title: "Ali Asif", description: "" },
+    { title: "", description: "Student | Developer | Designer | Notion Enthusiast" },
     {
-      title: "Ali Asif",
-      description: "Student | Developer | Designer | Notion Enthusiast",
+      title: "data",
+      description: "more data",
     },
     { title: "", description: "" },
   ];
 
   const secondRowLeftTopSections = [
-    { title: "Top Left 1", description: "First half of top left" },
-    { title: "Top Left 2", description: "Second half of top left" },
+    {title: "Top Left 1", description: "First half of top left"},
+    {title: "Top Left 2", description: "Second half of top left"},
   ];
 
   const secondRowLeftBottomSections = [
@@ -61,7 +59,7 @@ const About: React.FC<AboutProps> = ({
             >
               <div className="absolute inset-0 bg-gradient-to-br from-white/10 to-transparent opacity-20 pointer-events-none"></div>
               {section.title && (
-                <h2 className="text-xl font-bold mb-2 relative z-10 text-white text-left">
+                <h2 className="text-7xl font-bold mb-2 relative z-10 text-white text-left">
                   {section.title}
                 </h2>
               )}
@@ -190,61 +188,60 @@ const About: React.FC<AboutProps> = ({
       </div>
 
       {/* Third Row */}
-<div
-  className="flex space-x-4 w-full"
-  style={{ height: `${thirdRowHeight}px` }}
->
-  <div className="w-1/4 flex flex-col items-center justify-center p-4 rounded-xl shadow-2xl h-full backdrop-blur-2xl bg-white/10 border border-white/20 relative overflow-hidden">
-    <div className="absolute inset-0 bg-gradient-to-br from-white/10 to-transparent opacity-20 pointer-events-none"></div>
-    <h2 className="text-xl font-bold mb-2 relative z-10 text-white">
-      {thirdRowSections[0].title}
-    </h2>
-    <p className="text-center relative z-10 text-white/80">
-      {thirdRowSections[0].description}
-    </p>
-  </div>
+      <div
+        className="flex space-x-4 w-full"
+        style={{ height: `${thirdRowHeight}px` }}
+      >
+        <div className="w-1/4 flex flex-col items-center justify-center p-4 rounded-xl shadow-2xl h-full backdrop-blur-2xl bg-white/10 border border-white/20 relative overflow-hidden">
+          <div className="absolute inset-0 bg-gradient-to-br from-white/10 to-transparent opacity-20 pointer-events-none"></div>
+          <h2 className="text-xl font-bold mb-2 relative z-10 text-white">
+            {thirdRowSections[0].title}
+          </h2>
+          <p className="text-center relative z-10 text-white/80">
+            {thirdRowSections[0].description}
+          </p>
+        </div>
 
-  <div className="w-1/4 flex flex-col items-center justify-center p-4 rounded-xl shadow-2xl h-full backdrop-blur-2xl bg-white/10 border border-white/20 relative overflow-hidden">
-    <div className="absolute inset-0 bg-gradient-to-br from-white/10 to-transparent opacity-20 pointer-events-none"></div>
-    <h2 className="text-xl font-bold mb-2 relative z-10 text-white">
-      {thirdRowSections[1].title}
-    </h2>
-    <p className="text-center relative z-10 text-white/80">
-      {thirdRowSections[1].description}
-    </p>
-  </div>
+        <div className="w-1/4 flex flex-col items-center justify-center p-4 rounded-xl shadow-2xl h-full backdrop-blur-2xl bg-white/10 border border-white/20 relative overflow-hidden">
+          <div className="absolute inset-0 bg-gradient-to-br from-white/10 to-transparent opacity-20 pointer-events-none"></div>
+          <h2 className="text-xl font-bold mb-2 relative z-10 text-white">
+            {thirdRowSections[1].title}
+          </h2>
+          <p className="text-center relative z-10 text-white/80">
+            {thirdRowSections[1].description}
+          </p>
+        </div>
 
-  <div className="w-1/2 flex flex-col items-center justify-center p-4 rounded-xl shadow-2xl h-full backdrop-blur-2xl bg-white/10 border border-white/20 relative overflow-hidden">
-    <div className="absolute inset-0 bg-gradient-to-br from-white/10 to-transparent opacity-20 pointer-events-none"></div>
-    <h2 className="text-xl font-bold mb-2 relative z-10 text-white">
-      {thirdRowSections[2].title}
-    </h2>
-    <p className="text-center relative z-10 text-white/80">
-      {thirdRowSections[2].description}
-    </p>
-  </div>
+        <div className="w-1/4 flex flex-col items-center justify-center p-4 rounded-xl shadow-2xl h-full backdrop-blur-2xl bg-white/10 border border-white/20 relative overflow-hidden">
+          <div className="absolute inset-0 bg-gradient-to-br from-white/10 to-transparent opacity-20 pointer-events-none"></div>
+          <h2 className="text-xl font-bold mb-2 relative z-10 text-white">
+            {thirdRowSections[2].title}
+          </h2>
+          <p className="text-center relative z-10 text-white/80">
+            {thirdRowSections[2].description}
+          </p>
+        </div>
 
-  <div className="w-1/4 flex flex-col items-center justify-center p-4 rounded-xl shadow-2xl h-full backdrop-blur-2xl bg-white/10 border border-white/20 relative overflow-hidden">
-    <div className="absolute inset-0 bg-gradient-to-br from-white/10 to-transparent opacity-20 pointer-events-none"></div>
-    <h2 className="text-xl font-bold mb-2 relative z-10 text-white">
-      {thirdRowSections[3].title}
-    </h2>
-    <p className="text-center relative z-10 text-white/80">
-      {thirdRowSections[3].description}
-    </p>
-  </div>
+        <div className="w-1/4 flex flex-col items-center justify-center p-4 rounded-xl shadow-2xl h-full backdrop-blur-2xl bg-white/10 border border-white/20 relative overflow-hidden">
+          <div className="absolute inset-0 bg-gradient-to-br from-white/10 to-transparent opacity-20 pointer-events-none"></div>
+          <h2 className="text-xl font-bold mb-2 relative z-10 text-white">
+            {thirdRowSections[3].title}
+          </h2>
+          <p className="text-center relative z-10 text-white/80">
+            {thirdRowSections[3].description}
+          </p>
+        </div>
 
-  <div className="w-1/4 flex flex-col items-center justify-center p-4 rounded-xl shadow-2xl h-full backdrop-blur-2xl bg-white/10 border border-white/20 relative overflow-hidden">
-    <div className="absolute inset-0 bg-gradient-to-br from-white/10 to-transparent opacity-20 pointer-events-none"></div>
-    <h2 className="text-xl font-bold mb-2 relative z-10 text-white">
-      {thirdRowSections[4].title}
-    </h2>
-    <p className="text-center relative z-10 text-white/80">
-      {thirdRowSections[4].description}
-    </p>
-  </div>
-</div>
-
+        <div className="w-1/4 flex flex-col items-center justify-center p-4 rounded-xl shadow-2xl h-full backdrop-blur-2xl bg-white/10 border border-white/20 relative overflow-hidden">
+          <div className="absolute inset-0 bg-gradient-to-br from-white/10 to-transparent opacity-20 pointer-events-none"></div>
+          <h2 className="text-xl font-bold mb-2 relative z-10 text-white">
+            {thirdRowSections[4].title}
+          </h2>
+          <p className="text-center relative z-10 text-white/80">
+            {thirdRowSections[4].description}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
