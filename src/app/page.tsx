5dc20f603f6e641754d6c9a68d7684f266c1c7f6
Ali Asif <ali.asifkhatry@gmail.com>
2025-07-30 12:38:46 +0500

    deployment bugs blog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -207,7 +207,7 @@ export default function Home() {
           </h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             {recentBlogs.map((blog) => (
-              <BlogCard key={blog.slug} blog={blog} />
+              <BlogCard key={blog.mediumUrl} blog={blog} />
             ))}
           </div>
           <div className="mt-10 text-center">
