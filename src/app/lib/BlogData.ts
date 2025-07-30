bc37f5b34dafa912097eb2189299d7c9501e320c
Ali Asif <ali.asifkhatry@gmail.com>
2025-07-30 17:29:05 +0500

    blog 2

--- src/app/lib/BlogData.ts
+++ src/app/lib/BlogData.ts
@@ -7,6 +7,9 @@ export type BlogPost = {
 const blogPosts: BlogPost[] = [
   {
     mediumUrl: "https://medium.com/@aliasifkhatri/lets-see-if-i-can-write-who-i-am-11755b80ed87"
+  },
+  {
+    mediumUrl: "https://medium.com/@aliasifkhatri/my-first-model-un-experience-78f45c1bb0e7"
   }
 ];
 
