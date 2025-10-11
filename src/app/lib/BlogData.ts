4b0fc0c7b03b5617b64455ada23e7c02f8731391
Ali Asif <ali.asifkhatry@gmail.com>
2025-10-11 15:19:38 +0500

    new blog

--- src/app/lib/BlogData.ts
+++ src/app/lib/BlogData.ts
@@ -11,28 +11,33 @@ const blogPosts: BlogPost[] = [
   {
     mediumUrl: "https://medium.com/@aliasifkhatri/lets-see-if-i-can-write-who-i-am-11755b80ed87",
     title: "Let's See If I Can Write Who I Am",
-    description: "First few lines or summary of your blog goes here.",
+    description: "Hello there, this is Ali Asif, from Karachi, Pakistan. Not a great writer and no expertise, but the idea of having an article About Me just fascinated me enough to write this down.",
     image: "/blog-covers/blogcover1.webp"
   },
   {
     mediumUrl: "https://medium.com/@aliasifkhatri/my-first-model-un-experience-78f45c1bb0e7",
     title: "My First Model UN Experience",
-    description: "First few lines or summary of your blog goes here.",
+    description: "In the last week, I got the opportunity to participate in a Model United Nations Conference. It was hosted at the Heuser College on 23rd and 24th July.",
     image: "/blog-covers/blogcover2.webp"
+  },
+  {
+    mediumUrl: "https://medium.com/@aliasifkhatri/the-paradox-of-faking-7bb9bb3089ac",
+    title: "The Paradox of Faking",
+    description: "Throughout the history of mankind, the society has evolved and so has the “societal acceptance standard”, well I wasn't a part of that history, but yes, being a part of this current society we can observe a weird paradox today.",
+    image: "/blog-covers/blogcover6.webp"
   },
   {
     mediumUrl: "https://medium.com/@aliasifkhatri/ios-26-a-close-view-and-a-concern-1ffe45a4a608",
     title: "iOS 26: A Close View and a Concern",
-    description: "First few lines or summary of your blog goes here.",
+    description: "After signing up for the beta program of Apple, I was able to download iOS 26 on my phone. There were many new things, many old ones, just rephrased to look new and some unnecessary things.",
     image: "/blog-covers/blogcover3.webp"
   },
   {
     mediumUrl: "https://medium.com/@aliasifkhatri/new-developers-and-ai-3b7ec811408d",
     title: "New Developers and AI",
-    description: "First few lines or summary of your blog goes here.",
+    description: "after the boom of AI, we have become lazy while coding. First the AI was restricted to its app or the browser so it was still in control, as we had to do an effort to copy the code and paste it to get the solution and then adjust it in our file and all, so we preferred small fixes to be done on own.",
     image: "/blog-covers/blogcover4.webp"
-  }
-  ,
+  },
   {
     mediumUrl: "https://medium.com/@aliasifkhatri/winning-the-best-delegate-in-my-first-unsc-b5b39daf84af",
     title: "Winning the Best Delegate in My First UNSC",
